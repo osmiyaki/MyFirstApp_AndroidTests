dc12e46ef6e6d3e9e6bfb0c02a5edd318df120b6
Oscar Sabaleta Miyaki <kioscar28@hotmail.com>
2015-12-16 17:29:53 -0600

    First App Google Android Studio Tutorial - Receive the Intent

--- app/src/main/java/osmiyaki/myfirstapp/DisplayMessageActivity.java
+++ app/src/main/java/osmiyaki/myfirstapp/DisplayMessageActivity.java
@@ -1,5 +1,6 @@
 package osmiyaki.myfirstapp;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -12,7 +13,8 @@ public class DisplayMessageActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_display_message);
+        Intent intent = getIntent();
+        String message = intent.getStringExtra(MyActivity.EXTRA_MESSAGE);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
@@ -24,7 +26,7 @@ public class DisplayMessageActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        //getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
 
 }
